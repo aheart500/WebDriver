01062b081fc62e9c45f0a1ad0a9e5a3371b5798e
Mohamed Nasser <aheart500@yahoo.com>
2016-01-03 10:33:29 +0200

    First Commit.

---
+++ WebDriver/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace WebDriver
+{
+    [TestClass]
+    public class WikipediaSearchTest
+    {
+        [TestMethod]
+        public void Search_FindResults_ResultsIsShown()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            driver.Navigate().GoToUrl("http://en.wikipedia.org/wiki/Main_Page");
+         IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
+            searchInput.SendKeys("Christiaan Barnard");
+            
+        }
+    }
+}
